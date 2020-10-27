48396b6d8211bf56ae3068fdffd34e9d3ea5aff9
Hoon Huh <closelogin@hanmail.net>
2020-10-27 18:14:36 +0900

    scan and stop logic has been added (#5)
    Co-authored-by: hoon <hoon@getmiso.com>

--- lib/scene/component/ble_devices.dart
+++ lib/scene/component/ble_devices.dart
@@ -5,6 +5,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'package:permission_handler/permission_handler.dart';
 import 'package:pawn/service/ble.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
 
 class BleDevicesWidget extends HookWidget {
   @override
@@ -38,7 +40,7 @@ class BleDevicesWidget extends HookWidget {
         itemBuilder: (context, index) {
           return ListTile(
             //디바이스 이름과 맥주소 그리고 신호 세기를 표시한다.
-            title: Text(deviceList[index].deviceName),
+            title: Text('${index} : ${deviceList[index].deviceName}'),
             subtitle: Text(deviceList[index].peripheral.identifier),
             trailing: Text("${deviceList[index].rssi}"),
           );
@@ -49,14 +51,58 @@ class BleDevicesWidget extends HookWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('ble scan'),
-    ),
-    body: Center(
-    child: list(),
-    ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: service.doScan,
-        child: Icon(Icons.bluetooth_searching),
       ),
+      body: Center(
+        child: list(),
+      ),
+      floatingActionButton: SpeedDial(
+        // both default to 16
+        marginRight: 18,
+        marginBottom: 20,
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        // this is ignored if animatedIcon is non null
+        // child: Icon(Icons.add),
+        visible: true,
+        // If true user is forced to close dial manually
+        // by tapping main button and overlay is not rendered.
+        closeManually: false,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        onOpen: () => print('OPENING DIAL'),
+        onClose: () => print('DIAL CLOSED'),
+        tooltip: 'Speed Dial',
+        heroTag: 'speed-dial-hero-tag',
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 8.0,
+        shape: CircleBorder(),
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.bluetooth_searching),
+              backgroundColor: Colors.red,
+              label: 'First',
+              labelStyle: TextStyle(fontSize: 18.0),
+              onTap: () => service.doScan()
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.bluetooth_disabled),
+            backgroundColor: Colors.blue,
+            label: 'Second',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => service.stopScan(),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.keyboard_voice),
+            backgroundColor: Colors.green,
+            label: 'Third',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('THIRD CHILD'),
+          ),
+        ],
+      ),
+
     );
   }
 }
